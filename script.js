b9c571a999e92578b3ff69f6b5a19dd1281dd149
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-04-26 20:55:39 +0330

    finished job

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const cards=document.querySelectorAll('.card');
+const headings=document.querySelectorAll('.heading');
 
 cards.forEach((card)=>{
     card.addEventListener('click',()=>{
         cards.forEach((card)=>{
             card.classList.remove('active');
         })
+        
+        headings.forEach((heading)=>{
+            heading.classList.remove("active")
+        })
+        headings.forEach((heading)=>{
+            heading.classList.remove("show")
+        })
+
         card.classList.add('active');
+
+        card.childNodes[1].classList.add("show");
     })
 })
+
